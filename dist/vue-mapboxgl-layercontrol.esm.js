da4c675123e3fed2bb30ef006d3f67f7f0d0d15d
Fedor Baart <fedor.baart@deltares.nl>
2018-12-23 16:07:19 +0100

    Layer Control Package

---
+++ dist/vue-mapboxgl-layercontrol.esm.js
@@ -0,0 +1,208 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import draggable from 'vuedraggable';
+
+//
+
+Vue.use(Vuetify);
+
+var script = {
+  name: 'vue-mapboxgl-layercontrol', // vue component name
+  props: {
+    map: {
+      type: Object
+    },
+    layers: {
+      type: Array,
+      default: []
+    }
+  },
+  data: function data() {
+    return {
+    }
+  },
+  components: {
+    draggable: draggable
+  }
+};
+
+/* script */
+            var __vue_script__ = script;
+            
+/* template */
+var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"vue-mapboxgl-layercontrol"},[_c('v-list',[_c('draggable',{attrs:{"options":{handle: '.handle'}},on:{"start":function($event){_vm.drag=true;},"end":function($event){_vm.drag=false;}},model:{value:(_vm.myArray),callback:function ($$v) {_vm.myArray=$$v;},expression:"myArray"}},_vm._l((_vm.layers),function(layer){return _c('v-list-group',{key:layer.title,attrs:{"no-action":""},on:{"click":function($event){}},model:{value:(layer.expanded),callback:function ($$v) {_vm.$set(layer, "expanded", $$v);},expression:"layer.expanded"}},[_c('v-list-tile',{staticClass:"header",attrs:{"slot":"activator","avatar":"","dense":""},on:{"click":function($event){$event.stopPropagation();}},slot:"activator"},[_c('v-icon',{staticClass:"handle"},[_vm._v("drag_indicator")]),_vm._v(" "),_c('v-list-tile-action',{staticClass:"toggle"},[_c('v-checkbox',{model:{value:(layer.active),callback:function ($$v) {_vm.$set(layer, "active", $$v);},expression:"layer.active"}})],1),_vm._v(" "),_c('v-list-tile-content',[_c('v-list-tile-title',{domProps:{"textContent":_vm._s(layer.name)}})],1),_vm._v(" "),_c('v-list-tile-avatar',[_c('img',{attrs:{"src":layer.icon}})])],1),_vm._v(" "),_c('v-container',{attrs:{"fluid":"","pa-2":""}},[_c('v-layout',{attrs:{"row":"","wrap":""}},[_c('v-flex',{attrs:{"xs12":""}},[_c('v-slider',{attrs:{"label":"opacity","height":"20px"},model:{value:(layer.opacity),callback:function ($$v) {_vm.$set(layer, "opacity", $$v);},expression:"layer.opacity"}})],1)],1)],1)],1)}),1)],1)],1)};
+var __vue_staticRenderFns__ = [];
+
+  /* style */
+  var __vue_inject_styles__ = function (inject) {
+    if (!inject) { return }
+    inject("data-v-cd81072a_0", { source: ".vue-mapboxgl-layercontrol[data-v-cd81072a] .header>.v-list__tile{padding:0 6px}.v-list__group__header .v-list__group__header__prepend-icon[data-v-cd81072a],.vue-mapboxgl-layercontrol[data-v-cd81072a] .v-list__group__header .v-list__group__header__append-icon{padding:0 6px}.vue-mapboxgl-layercontrol[data-v-cd81072a] .v-input--slider{margin-top:inherit}.toggle.v-list__tile__action[data-v-cd81072a]{min-width:inherit}.handle[data-v-cd81072a]:hover{cursor:move}", map: undefined, media: undefined });
+
+  };
+  /* scoped */
+  var __vue_scope_id__ = "data-v-cd81072a";
+  /* module identifier */
+  var __vue_module_identifier__ = undefined;
+  /* functional template */
+  var __vue_is_functional_template__ = false;
+  /* component normalizer */
+  function __vue_normalize__(
+    template, style, script$$1,
+    scope, functional, moduleIdentifier,
+    createInjector, createInjectorSSR
+  ) {
+    var component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
+
+    // For security concerns, we use only base name in production mode.
+    component.__file = "vue-mapboxgl-layercontrol.vue";
+
+    if (!component.render) {
+      component.render = template.render;
+      component.staticRenderFns = template.staticRenderFns;
+      component._compiled = true;
+
+      if (functional) { component.functional = true; }
+    }
+
+    component._scopeId = scope;
+
+    {
+      var hook;
+      if (style) {
+        hook = function(context) {
+          style.call(this, createInjector(context));
+        };
+      }
+
+      if (hook !== undefined) {
+        if (component.functional) {
+          // register for functional component in vue file
+          var originalRender = component.render;
+          component.render = function renderWithStyleInjection(h, context) {
+            hook.call(context);
+            return originalRender(h, context)
+          };
+        } else {
+          // inject component registration as beforeCreate hook
+          var existing = component.beforeCreate;
+          component.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+        }
+      }
+    }
+
+    return component
+  }
+  /* style inject */
+  function __vue_create_injector__() {
+    var head = document.head || document.getElementsByTagName('head')[0];
+    var styles = __vue_create_injector__.styles || (__vue_create_injector__.styles = {});
+    var isOldIE =
+      typeof navigator !== 'undefined' &&
+      /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
+
+    return function addStyle(id, css) {
+      if (document.querySelector('style[data-vue-ssr-id~="' + id + '"]')) { return } // SSR styles are present.
+
+      var group = isOldIE ? css.media || 'default' : id;
+      var style = styles[group] || (styles[group] = { ids: [], parts: [], element: undefined });
+
+      if (!style.ids.includes(id)) {
+        var code = css.source;
+        var index = style.ids.length;
+
+        style.ids.push(id);
+
+        if (css.map) {
+          // https://developer.chrome.com/devtools/docs/javascript-debugging
+          // this makes source maps inside style tags work properly in Chrome
+          code += '\n/*# sourceURL=' + css.map.sources[0] + ' */';
+          // http://stackoverflow.com/a/26603875
+          code +=
+            '\n/*# sourceMappingURL=data:application/json;base64,' +
+            btoa(unescape(encodeURIComponent(JSON.stringify(css.map)))) +
+            ' */';
+        }
+
+        if (isOldIE) {
+          style.element = style.element || document.querySelector('style[data-group=' + group + ']');
+        }
+
+        if (!style.element) {
+          var el = style.element = document.createElement('style');
+          el.type = 'text/css';
+
+          if (css.media) { el.setAttribute('media', css.media); }
+          if (isOldIE) {
+            el.setAttribute('data-group', group);
+            el.setAttribute('data-next-index', '0');
+          }
+
+          head.appendChild(el);
+        }
+
+        if (isOldIE) {
+          index = parseInt(style.element.getAttribute('data-next-index'));
+          style.element.setAttribute('data-next-index', index + 1);
+        }
+
+        if (style.element.styleSheet) {
+          style.parts.push(code);
+          style.element.styleSheet.cssText = style.parts
+            .filter(Boolean)
+            .join('\n');
+        } else {
+          var textNode = document.createTextNode(code);
+          var nodes = style.element.childNodes;
+          if (nodes[index]) { style.element.removeChild(nodes[index]); }
+          if (nodes.length) { style.element.insertBefore(textNode, nodes[index]); }
+          else { style.element.appendChild(textNode); }
+        }
+      }
+    }
+  }
+  /* style inject SSR */
+  
+
+  
+  var component = __vue_normalize__(
+    { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+    __vue_inject_styles__,
+    __vue_script__,
+    __vue_scope_id__,
+    __vue_is_functional_template__,
+    __vue_module_identifier__,
+    __vue_create_injector__,
+    undefined
+  );
+
+// Import vue component
+
+// install function executed by Vue.use()
+function install(Vue$$1) {
+  if (install.installed) { return; }
+  install.installed = true;
+  Vue$$1.component('VueMapboxglLayercontrol', component);
+}
+
+// Create module definition for Vue.use()
+var plugin = {
+  install: install,
+};
+
+// To auto-install when vue is found
+/* global window global */
+var GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+// It's possible to expose named exports when writing components that can
+// also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
+// export const RollupDemoDirective = component;
+
+export default component;
---
+++ src/vue-mapboxgl-layercontrol.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="vue-mapboxgl-layercontrol" >
+    <v-list >
+      <draggable v-model="myArray" :options="{handle: '.handle'}" @start="drag=true" @end="drag=false">
+        <v-list-group
+          v-for="layer in layers"
+          v-model="layer.expanded"
+          :key="layer.title"
+          @click=""
+          no-action
+          >
+          <v-list-tile
+            avatar
+            dense
+            @click.stop=""
+            slot="activator"
+            class="header"
+            >
+            <v-icon class="handle">drag_indicator</v-icon>
+            <v-list-tile-action class="toggle">
+              <v-checkbox v-model="layer.active"></v-checkbox>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-text="layer.name"></v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-avatar>
+              <img :src="layer.icon">
+            </v-list-tile-avatar>
+          </v-list-tile>
+
+
+          <v-container fluid pa-2>
+            <v-layout row wrap>
+              <v-flex xs12>
+                <v-slider v-model="layer.opacity" label="opacity" height="20px"></v-slider>
+              </v-flex>
+            </v-layout>
+          </v-container>
+
+
+
+
+        </v-list-group>
+
+      </draggable>
+    </v-list>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+import Vuetify from 'vuetify'
+import draggable from 'vuedraggable'
+
+Vue.use(Vuetify)
+
+export default {
+  name: 'vue-mapboxgl-layercontrol', // vue component name
+  props: {
+    map: {
+      type: Object
+    },
+    layers: {
+      type: Array,
+      default: []
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  components: {
+    draggable
+  }
+}
+          </script>
+
+<style scoped>
+  .vue-mapboxgl-layercontrol >>> .header>.v-list__tile {
+  padding: 0 6px;
+  }
+  .vue-mapboxgl-layercontrol >>> .v-list__group__header .v-list__group__header__append-icon, .v-list__group__header .v-list__group__header__prepend-icon {
+  padding: 0 6px;
+  }
+
+  .vue-mapboxgl-layercontrol >>> .v-input--slider {
+   margin-top: inherit;
+  }
+
+  .toggle.v-list__tile__action {
+  min-width: inherit;
+  }
+  .handle:hover {
+  cursor: move;
+  }
+</style>
